31ca1bd16529ad96810ef75469594052f5cb1747
alexrg <alerodriguezgarcia@me.com>
2019-03-22 21:18:16 -0600

    Creates a list with a length chosen in the argument, that has random numbers from 0 to 50

--- lists/eliminate_duplicate.py
+++ lists/eliminate_duplicate.py
@@ -1,3 +1,5 @@
+import random 
+
 """
 Given a sorted linked list, delete all duplicates such that each element
 appear only once.
@@ -19,3 +21,33 @@ def eliminate_duplicates(the_list):
 			unique_elements.append(element)
 	
 	return unique_elements
+
+def create_list(length):
+	"""
+	Creates a list with a length chosen in the argument, that has random
+	numbers from 0 to 50
+
+	Args:
+		length: The length of the list that will be created
+
+	Returns:
+		list: A list with random elements from 0 to 50
+	"""
+	the_list = []
+	
+	for element in range(0,length):
+		element = random.randint(0, 50)
+		the_list.append(element)
+	
+	return the_list 
+
+
+print("Enter the length of the list you want to create: ")
+list_length = int(input())
+
+the_list = create_list(list_length)
+
+print("Your list: {}".format(the_list))
+
+unique_elements_list = eliminate_duplicates(the_list)
+print("Your list with unique elements: {}".format(unique_elements_list))
