5efd41805e3c6988ee2d466cc0cf97f4082bbd4c
Ale <alerodriguezgarcia@me.com>
2019-05-31 16:18:35 -0600

    Write a Python statement that calculates and prints the number of seconds in 7 hours, 21 minutes and 37 seconds.

--- math/basic_math/unit_converter.py
+++ math/basic_math/unit_converter.py
@@ -16,3 +16,21 @@ def miles_to_feet(miles):
 
 miles = 13
 feet = miles_to_feet(miles)
+
+
+"""
+Write a Python statement that calculates and prints the number of
+seconds in 7 hours, 21 minutes and 37 seconds.
+"""
+def time_to_seconds(hours, minutes, seconds):
+	hours_to_seconds = 60 * 60 * hours
+	minutes_to_seconds = 60 * minutes
+
+	total_seconds = hours_to_seconds + minutes_to_seconds + seconds
+
+	return total_seconds
+
+hours = 7
+minutes = 21
+seconds = 37
+print(time_to_seconds(hours, minutes, seconds))
