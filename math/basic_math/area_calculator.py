8e1610bab8cb2b2c822bd8feea778f59f8603188
Ale <alerodriguezgarcia@me.com>
2019-06-03 17:03:20 -0600

    Proper use of PEP-8 documentation in functions

--- math/basic_math/area_calculator.py
+++ math/basic_math/area_calculator.py
@@ -4,5 +4,16 @@ height corresponding to the lengths of the sides of a rectangle and returns
 the area of the rectangle in square inches
 """
 def rectangle_area(width, height):
+	"""
+	Calculate the area of a rectangle
+	Args:
+		width (number): Width of the rectangle
+		heigth (number): Heigth of the rectangle
+
+	Returns:
+		area (number): The calculated area of the rectangle
+	"""
 	area = width * height
 	return area
+
+rectangle = rectangle_area(4,5)
