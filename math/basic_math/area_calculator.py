84e5426db049dbe7358a8ca6c0657cb38844d2cc
Ale <alerodriguezgarcia@me.com>
2019-06-03 16:47:54 -0600

    Write a Python function rectangle_area that takes two parameters width and height corresponding to the lengths of the sides of a rectangle and returns the area of the rectangle in square inches

---
+++ math/basic_math/area_calculator.py
@@ -0,0 +1,8 @@
+"""
+Write a Python function rectangle_area that takes two parameters width and
+height corresponding to the lengths of the sides of a rectangle and returns
+the area of the rectangle in square inches
+"""
+def rectangle_area(width, height):
+	area = width * height
+	return area
