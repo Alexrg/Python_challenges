18a006595bfdac31dc8d72fc05ca12afa705e9bb
Ale <alerodriguezgarcia@me.com>
2019-06-04 10:28:55 -0600

    Write a Python function circle_area that takes a single parameter radius corresponding to the radius of a circle in inches and returns the the area of a circle with radius radius in square inches. Do not use π = 3.14, instead use the math module to supply a higher-precision approximation to π.

--- math/basic_math/area_calculator.py
+++ math/basic_math/area_calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 Write a Python function rectangle_area that takes two parameters width and
 height corresponding to the lengths of the sides of a rectangle and returns
@@ -17,3 +18,23 @@ def rectangle_area(width, height):
 	return area
 
 rectangle = rectangle_area(4,5)
+
+"""
+Write a Python function circle_area that takes a single parameter radius
+corresponding to the radius of a circle in inches and returns the the area of a
+circle with radius radius in square inches. Do not use π = 3.14, instead use the
+math module to supply a higher-precision approximation to π.
+"""
+def circle_area(radius):
+	"""
+	Calculate the area of a circle given the radius
+	Args:
+		radius (number): The radius of a circle
+
+	Returns:
+		area (number): The calculated area of the circle
+	"""
+	area = math.pi * math.pow(radius, 2)
+	return area
+
+circle = circle_area(35)
