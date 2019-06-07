3fcc78280f0caab16c91bce9103d1e3d3c72dee9
Ale <alerodriguezgarcia@me.com>
2019-06-07 17:23:18 -0600

    Calculate the area of a polygon given the number of sides and their length

--- math/basic_math/area_calculator.py
+++ math/basic_math/area_calculator.py
@@ -38,3 +38,32 @@ def circle_area(radius):
 	return area
 
 circle = circle_area(35)
+
+"""
+There are several ways to calculate the area of a regular polygon. Given the
+number of sides, nn, and the length of each side, s, the polygon's area is: 
+ns^2 / 4tan(n / π)
+​For example, a regular polygon with 5 sides, each of length 7 inches, has an
+area of 84.3033926289 square inches. Write a function that calculates the area
+of a regular polygon, given the number of sides and length of each side. Submit
+the area of a regular polygon with 7 sides each of length 3 inches. Enter a
+number (and not the units) with at least four digits of precision after the
+decimal point. Note that the use of inches as the unit of measurement in these
+examples is arbitrary. Python only keeps track of the numerical values, not
+the units.
+"""
+def polygon_area(number_of_sides, side_length):
+		"""
+	Calculate the area of a polygon given the number of sides and their length
+	Args:
+		number_of_sides (number): Number of sides of the polygon
+		side_length (number): Size of the polygon sides
+
+	Returns:
+		area (number): The calculated area of the polygon
+	"""
+	area = (number_of_sides * math.pow(side_length,2)) / (4 * math.tan(math.pi/number_of_sides))
+	return area
+
+polygon = polygon_area(5, 7)
+print("The area of a 5 side polygon is {}".format(polygon))
