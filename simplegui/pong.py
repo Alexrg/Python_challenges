edc132027f31d2b3a0c90d7b886ee17f2c248cce
Ale <alerodriguezgarcia@me.com>
2019-08-05 20:51:11 -0600

    Draw the elements of the pong game

--- simplegui/pong.py
+++ simplegui/pong.py
@@ -58,9 +58,13 @@ def new_game():
     Initiate a new Pong game spawning the ball in a random direction
 
     Global:
-        paddle1_pos (Number): The position of the paddle
-        ball_pos (List): The position of tha ball
-        ball_vell (List): The velocity of tha ball
+        paddle1_pos (Number): The position of the paddle in the left
+        paddle2_pos (Number): The position of the paddle in the right
+        paddle1_vel (Number): The velocity of the paddle in the left
+        paddle2_vel (Number): The velocity of the paddle in the right
+        score1 (Number): The score of the player in the left
+        score2 (Number): The score of the player in the right
+
     """
     global paddle1_pos
     global paddle2_pos
@@ -75,3 +79,77 @@ def new_game():
     direction = [LEFT, RIGHT]
 
     spawn_ball(direction[random.randint(0,1)])
+
+def draw(canvas):
+    """
+    Draw the elements of the pong game
+
+    Global:
+        paddle1_pos (Number): The position of the paddle in the left
+        paddle2_pos (Number): The position of the paddle in the right
+        paddle1_vel (Number): The velocity of the paddle in the left
+        paddle2_vel (Number): The velocity of the paddle in the right
+        ball_pos (List): The position of the ball in x and y coordinates
+        ball_vel (List): The velocity of the ball in x and y coordinates
+        score1 (Number): The score of the player in the left
+        score2 (Number): The score of the player in the right
+    """
+    
+    global paddle1_pos
+    global paddle2_pos
+    global paddle1_vel
+    global paddle2_vel
+    global ball_pos
+    global ball_vel
+    global score1
+    global score2  
+
+    
+    # draw mid line and gutters
+    canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
+    canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
+    canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
+    
+    # update ball
+    ball_pos[0] += ball_vel[0]
+    ball_pos[1] += ball_vel[1]
+    
+    # draw ball
+    canvas.draw_circle(ball_pos, BALL_RADIUS, 1, 'White', 'White')
+    
+    # update paddle's vertical position, keep paddle on the screen
+    paddle1_pos += paddle1_vel
+    paddle2_pos += paddle2_vel
+
+    if paddle1_pos > 300 or paddle1_pos < 0:
+        paddle1_vel = 0
+    if paddle2_pos > 300 or paddle2_pos < 0:
+        paddle2_vel = 0 
+    
+    # draw paddles
+    canvas.draw_line([0, paddle1_pos], [0, paddle1_pos+100], 15, 'Green')
+    canvas.draw_line([600, paddle2_pos], [600, paddle2_pos+100], 15, 'Red')
+    
+    # determine whether paddle and ball collide 
+    if ball_pos[0]-BALL_RADIUS <= PAD_WIDTH:
+        if paddle1_pos < ball_pos[1] and paddle1_pos + 100 > ball_pos[1]:
+            ball_vel[0] = -ball_vel[0]
+            ball_vel[0] *= 1.1
+            ball_vel[1] *= 1.1
+        else:
+            spawn_ball(LEFT)
+            score2 += 1
+    elif ball_pos[0]+BALL_RADIUS >= WIDTH-PAD_WIDTH:
+        if paddle2_pos < ball_pos[1] and paddle2_pos + 100 > ball_pos[1]:
+            ball_vel[0] = -ball_vel[0]
+            ball_vel[0] *= 1.1
+            ball_vel[1] *= 1.1
+        else:
+            spawn_ball(RIGHT)
+            score1 += 1       
+    elif ball_pos[1]-BALL_RADIUS <= 0 or ball_pos[1]+BALL_RADIUS >= HEIGHT:
+        ball_vel[1] = -ball_vel[1]
+    
+    # draw scores
+    canvas.draw_text("You: {}".format(score1), [20,10], 15, "Green")
+    canvas.draw_text("CPU: {}".format(score2), [530,10], 15, "Red")
