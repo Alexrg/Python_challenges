41edc06a1086ccf5ccb5f5a0b489758dafc0c076
Ale <alerodriguezgarcia@me.com>
2019-09-02 21:25:28 -0600

    Draw in the canvas with timers to build a simple digital stopwatch that keeps track of the time in tenths of a second. The stopwatch should contain Start, Stop and Reset buttons.

---
+++ simplegui/stop_watch.py
@@ -0,0 +1,16 @@
+import simplegui
+import time
+"""
+Our mini-project for this week will focus on combining text drawing in the
+canvas with timers to build a simple digital stopwatch that keeps track of the
+time in tenths of a second. The stopwatch should contain "Start", "Stop" and
+"Reset" buttons.
+
+This code can only be run in https://py3.codeskulptor.org/ since simplegui
+only works there. Create an interactive console thet greets the user.
+"""
+clock = 0
+stop_counter = 0
+seconds = 0
+success = 0
+attempts = 0
