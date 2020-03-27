ff311f74cdfd17cd690d6d4c59ff42c4c8a5f81d
Ale <alerodriguezgarcia@me.com>
2020-03-26 18:42:20 -0700

    Deletes unecessary descriptions and creates a method that Returns the full name for a person as a string, which is the first name followed by a space, followed by the last name.

--- mini_projects/person.py
+++ mini_projects/person.py
@@ -1,12 +1,3 @@
-"""
-As your first task, implement a Person class which has the fields first_name, last_name and birth_year.
-This class should include the methods: __init__ which takes strings for the two name fields and an
-integer for the year of birth, full_name returns the full name for a person as a string, which is
-the first name followed by a space, followed by the last name, age which takes the current year as
-input and returns the age in years of the person (Don't worry about days and months here, just return
-the difference of the two years.), and __str__ returns a string that includes the first name and last
-name of the person as well as their year of birth. Definition of Person class template
-"""
 class Person():
 	def __init__(self, first_name, last_name, birth_year):
 		"""
@@ -18,9 +9,22 @@ class Person():
 			last_name (string): The person's last name
 			birth_year (int): The person's birth year
 		"""
+		
 		self.first_name = first_name
 		self.last_name = last_name
 		self.birth_year = birth_year
 
+	def full_name(self):
+		"""
+		Returns the full name for a person as a string, which is the first name
+		followed by a space, followed by the last name
+
+		Returtns:
+			full_name (string): Full name of the person
+		"""
+		
+		full_name = "{} {}".format(self.first_name, self.last_name)
+		return full_name
+
 alejandra = Person("Alejandra", "Rodríguez",1992)
-print(alejandra.birth_year)
+print(alejandra.full_name())
