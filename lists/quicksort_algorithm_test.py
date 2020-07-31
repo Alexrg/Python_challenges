035716d6c1a90b24e7eac7e27be2d7241ef1cf37
Ale <alerodriguezgarcia@me.com>
2020-07-30 21:25:49 -0600

    Test if the number of elements created in the create_list method is correct.

---
+++ lists/quicksort_algorithm_test.py
@@ -0,0 +1,17 @@
+import unittest
+from quicksort_algorithm import create_list
+
+
+class TestStringMethods(unittest.TestCase):
+
+	def test_number_of_elements(self):
+		"""
+		Test if the number of elements created in the create_list method is correct.
+		"""
+		
+		list_elements = len(create_list())
+
+		self.assertEqual(list_elements, 10)
+
+if __name__ == '__main__':
+    unittest.main()
