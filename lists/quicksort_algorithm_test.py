60b913cd62eb1a34808ff692470d08364c5117c6
Ale <alerodriguezgarcia@me.com>
2021-05-26 15:29:19 -0600

    fix commits.

--- lists/quicksort_algorithm_test.py
+++ lists/quicksort_algorithm_test.py
@@ -2,11 +2,15 @@ import unittest
 from quicksort_algorithm import create_list
 
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 class TestCreateListMethod(unittest.TestCase):
 =======
 class TestStringMethods(unittest.TestCase):
 >>>>>>> 87f1d17806e8b10b341dbb7f95a97179aadf08d5
+=======
+class TestCreateListMethod(unittest.TestCase):
+>>>>>>> Stashed changes
 
 	def test_number_of_elements(self):
 		"""
@@ -17,9 +21,13 @@ class TestStringMethods(unittest.TestCase):
 
 		self.assertEqual(list_elements, 10)
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 
 =======
 >>>>>>> 87f1d17806e8b10b341dbb7f95a97179aadf08d5
+=======
+
+>>>>>>> Stashed changes
 if __name__ == '__main__':
     unittest.main()
