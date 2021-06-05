c611f4a092b927f4606aacc8aa7ded46a495b76b
Ale <alerodriguezgarcia@me.com>
2021-06-05 11:18:58 -0600

    Create a Quicksort class that will sort the number list.

--- lists/quicksort_algorithm.py
+++ lists/quicksort_algorithm.py
@@ -1,13 +1,25 @@
 import random
 
-def create_list():
+
+class Quicksort():
 	"""
-	Creates a 10 element list, with random numbers from 0 to 500.
-
-	Returns:
-		random_list (array): A list containing random numbers from 0 to 500.
+	QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and
+	partitions the given array around the picked pivot.
 	"""
+	def __init__(self):
+		self.integer_list = self.create_list()
 
-	random_list = random.sample(range(0, 500), 10)
+	def create_list(self):
+		"""
+		Creates a 10 element list, with random numbers from 0 to 500.
 
-	return random_list
+		Returns:
+			random_list (array): A list containing random numbers from 0 to 500.
+		"""
+
+		random_list = random.sample(range(0, 500), 10)
+
+		return random_list
+
+number_list = Quicksort().integer_list
+print(number_list)
