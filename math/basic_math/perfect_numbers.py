b1cc235120776eec4822f20b524d099582576c7c
Ale <alerodriguezgarcia@me.com>
2021-08-19 19:56:03 -0600

    Classifies the given number. If the sum of the divisors is larger than the number, it's an abundant number, if the sum is smaller, it's called a deficient number, but if the sum is equal to the number, then it's a perfect number.

--- math/basic_math/perfect_numbers.py
+++ math/basic_math/perfect_numbers.py
@@ -27,6 +27,12 @@ def divisors(integer):
 def sum_numbers(divisors_list):
 	"""
 	Sum the numbers of the given list.
+    
+    Args:
+    	divisors_list (list): The list of the divisors of a number.
+
+    Returns:
+    	total_sum (number): Sum the numbers of the given list.	
 	"""
 	total_sum = 0
 
@@ -35,6 +41,27 @@ def sum_numbers(divisors_list):
 
 	return total_sum
 
-get_divisors = divisors(6)
-print(get_divisors)
-print(sum_numbers(get_divisors))
+def classify_number(number):
+	"""
+	Classifies the given number. If the sum of the divisors is larger than the
+	number, it's an abundant number, if the sum is smaller, it's called an
+	deficient number, but if the sum is equal to the number, then it's a
+	perfect number.
+    
+    Args:
+    	number (int): A given number.
+
+    Returns:
+    	(string): Prompts the clasification of the number.
+	"""
+	get_divisors = divisors(number)
+	sum_divisors = sum_numbers(get_divisors)
+	
+	if sum_divisors > number:
+		return "%d is an abundant number" % number
+	elif sum_divisors == number:
+		return "%d is a perfect number" % number
+	elif sum_divisors < number:
+		return "%d is a deficient number" % number
+
+print(classify_number(8128))
