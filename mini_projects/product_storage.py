057af4e064924c90acffb3484afa866f615a0609
Ale <alerodriguezgarcia@me.com>
2021-08-23 19:27:07 -0600

    Create a program that stores product information, as an object, when filing the product, you must ask if one more is added, the products will be stored as a tuple of objects, in the end all products will be traversed and printed on screen. Creates an object class that stores the product's name and price.

---
+++ mini_projects/product_storage.py
@@ -0,0 +1,20 @@
+"""
+Create a program that stores product information, as an object, when filing the
+product, you must ask if one more is added, the products will be stored as a
+tuple of objects, in the end all products will be traversed and printed
+on screen.
+
+1. The amount of features to add is free.
+2. Must be only one file.
+3. PEP 8 must be used.
+"""
+class Product(object):
+	"""
+	Object class of a product. Stores the name and price.
+	"""
+	name = ""
+	price = 0.0
+
+	def __init__(self, name, price):
+		self.name = name
+		self.price = price
