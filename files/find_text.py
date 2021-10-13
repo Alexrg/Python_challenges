acba8ebbc82556afde5f2e672f2b07a492db84a4
Ale <alerodriguezgarcia@me.com>
2021-10-12 21:52:52 -0600

    Returns True if the texts is in the file, but False if it is not.

--- files/find_text.py
+++ files/find_text.py
@@ -20,7 +20,16 @@ def extract_text_from_file(file_name):
 
 	return file_text
 
+def evaluate_string_in_text():
+	"""
+	Returns "True" if the texts is in the file, but "False" if it is not.
 
-file_name = 'donquijote_capitulouno.txt'
+	Returns:
+		(boolean): "True" if the texts is in the file, but "False" if it is not.
+	"""
+	file_name = 'donquijote_capitulouno.txt'
+	file_text = extract_text_from_file(file_name=file_name)
 
-print(extract_text_from_file(file_name=file_name))
+	return "En un lugar de la Mancha, de cuyo nombre no quiero acordarme" in file_text
+
+print(evaluate_string_in_text())
