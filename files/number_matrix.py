f758744f78211aefd1a3c19da5a3739e1a6bc5d2
Ale <alerodriguezgarcia@me.com>
2021-10-13 19:30:53 -0600

    Receives the name's file to open it and exctrat the numbers matrix, to then store it in a variable.

---
+++ files/number_matrix.py
@@ -0,0 +1,32 @@
+"""
+Give a Comma Separated File (csv) and a column number (zero being the left most
+column) return the sum of all entries in that column. Assume that all the
+entries in the CSV are numbers. Assume also that there are no column headers.
+For example a file looks like this:
+1,2,3,4
+5,6,7,8
+9,10,11,12
+the sum of the column 0 will be 1 + 5 + 9 = 15 and the sum of the column 2 will
+be 3 + 7 + 11 = 21
+"""
+import pandas
+import numpy
+
+def extract_matrix_number(file_name):
+	"""
+	Receives the name's file to open it and exctrat the numbers matrix, to then store it
+	in a variable.
+
+	Args:
+		file_name (string): The name file
+
+	Returns:
+		file_text (string): The text from the file
+	"""
+	file = pandas.read_csv(file_name, header=None)
+	file_matrix = file.to_numpy()
+
+	return file_matrix
+
+file_name = "matrix_number.csv"
+print(extract_matrix_number(file_name))
