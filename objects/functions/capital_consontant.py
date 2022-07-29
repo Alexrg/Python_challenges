8d829e4e38f7b623ea2e823700fc3504cee7c52a
Alejandra <alerodriguezgarcia@me.com>
2022-07-28 21:43:57 -0600

    Takes as input a string, and return as output a single integer. The number the function returns should be the count of characters from the string that were capital consonants. For this problem, consider Y a consonant.

--- objects/functions/capital_consontant.py
+++ objects/functions/capital_consontant.py
@@ -9,3 +9,29 @@
 # count_capital_consonants("Georgia Tech") -> 2
 # count_capital_consonants("GEORGIA TECH") -> 6
 # count_capital_consonants("gEOrgIA tEch") -> 0
+
+#Write your function here!
+def count_capital_consonants(given_string):
+    """
+    Takes as input a string, and return as output 
+    a single integer. The number the function returns
+    should be the count of characters from the string
+    that were capital consonants. For this problem,
+    consider Y a consonant.
+    
+    Args:
+        given_string (string): An input string.
+        
+    Returns:
+        capital_consonant_count (int): the count of
+        characters from the string that were capital
+        consonants
+    """
+    capital_consonant_count = 0
+    vowels = ["A","E","I","O","U"]
+    
+    for letter in given_string:
+        if letter.isupper() and letter not in vowels:
+            capital_consonant_count += 1
+    
+    return capital_consonant_count
